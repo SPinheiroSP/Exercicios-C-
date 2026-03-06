bd35bc53d59802575e20befe26a143b15fb121bd
SPinheiroSP <sergiopinheiro.trader@gmail.com>
2026-03-06 00:02:40 +0000

    Exercício com cálculo de densidade populacional e renda per cápita das cartas

---
+++ Exercício Intermediário.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+int main() {
+
+    // Variáveis referente a carta 01;
+    
+    int população1; // variável de numero de habitantes;
+    double area1; // variável da area da cidade;
+    double pib1; // variável ddo pib da cidade;
+    int turism1; // variável de numero de pontos turísticos;
+    char code1[10]; // variável do código da cidade na carta;
+    char cidade1[30]; // variável do nome da cidade;
+    char estado1[5]; // variável do estado da cidade;
+    float densidade1, pibpercapita1; // variável de densidade populacional e renda per cápita;
+
+    // Campo de imput manual dos valores das variáveis;
+  
+    printf("Digite o Estado Referente a Carta 1:\n");
+    scanf("%s", &estado1);
+    printf("Digite o Código da Carta Referente a Carta 1:\n");
+    scanf("%s", &code1);
+    printf("Digite o Nome da Cidade Referente a Carta 1:\n");
+    scanf("%s", &cidade1);
+    printf("Digite o Numero de Habitantes Referente a Carta 1:\n");
+    scanf("%d", &população1);
+    printf("Digite a Área da Cidade em km Referente a Carta 1:\n");
+    scanf("%lf", &area1);
+    printf("Digite o PIB da Cidade Referente a Carta 1:\n");
+    scanf("%lf", &pib1);
+    printf("Digite o Número de Pontos Turísticos Referente a Carta 1:\n");
+    scanf("%d", &turism1);
+
+    densidade1 = (população1 / area1);
+    pibpercapita1 = (pib1 / população1);
+    
+    //Apresentação dos dados da Carta 01;
+
+    printf("Carta 1:\n");
+    printf("Estado: %s\n", estado1);
+    printf("Código: %s\n", code1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("População: %d\n", população1);
+    printf("Área: %.2lf km²\n", area1);
+    printf("PIB: R$%.2lf\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", turism1);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Cápita: R$%.2f\n", pibpercapita1);
+
+    // Variáveis referente a carta 02;
+    
+    int população2; // variável de numero de habitantes;
+    float area2; // variável da area da cidade,
+    float pib2; // variável ddo pib da cidade;
+    int turism2; // variável de numero de pontos turísticos;
+    char code2[10]; // variável do código da cidade na carta;
+    char cidade2[30]; // variável do nome da cidade;
+    char estado2[5]; // variável do estado da cidade;
+    float densidade2, pibpercapita2; // variável de densidade populacional e renda per cápita;
+
+    // Campo de imput manual dos valores das variáveis;
+  
+    printf("Digite o Estado Referente a Carta 2:\n");
+    scanf("%s", &estado2);
+    printf("Digite o Código da Carta Referente a Carta 2:\n");
+    scanf("%s", &code2);
+    printf("Digite o Nome da Cidade Referente a Carta 2:\n");
+    scanf("%s", &cidade2);
+    printf("Digite o Numero de Habitantes Referente a Carta 2:\n");
+    scanf("%d", &população2);
+    printf("Digite a Área da Cidade em km Referente a Carta 2:\n");
+    scanf("%f", &area2);
+    printf("Digite o PIB da Cidade Referente a Carta 2:\n");
+    scanf("%f", &pib2);
+    printf("Digite o Número de Pontos Turísticos Referente a Carta 2:\n");
+    scanf("%d", &turism2);
+
+    densidade2 = (população2 / area2);
+    pibpercapita2 = (pib2 / população2);
+
+    //Apresentação dos dados da Carta 02;
+
+    printf("Carta 2:\n");
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", code2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("População: %d\n", população2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: R$%.2f\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", turism2);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Cápita: R$%.2f\n", pibpercapita2);
+
+}
